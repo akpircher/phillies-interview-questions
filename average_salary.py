9023eef0b9eb79eab04afeed3e0e6ef5be94fbaa
Amy Pircher <akpircher@gmail.com>
2018-08-15 10:56:20 -0700

    Fixed CLI code, added shortcut for running basic script

--- average_salary.py
+++ average_salary.py
@@ -17,12 +17,8 @@ def filter_year(df, year=2016):
     return df.loc[df.Year == year]
 
 
-def top_salaries(df, top=125, year=2016, duplicates=True):
-    # Filter the salaries for just this year
-    this_year = df.loc[df.Year == year]
-    if duplicates:
-        return this_year.Salary.sort_values(ascending=False).head(top)
-    return this_year.Salary.nlargest(top)
+def top_salaries(df, top=125):
+    return df.Salary.nlargest(top)
 
 
 def average(salaries):
@@ -37,10 +33,10 @@ if __name__ == '__main__':
     # Retrieve the salary data from the database
     df = get_salary_data()
 
-    filtered = filter_year(df, df.Year.unique().max())
+    filtered = filter_year(df, df.Year.max())
 
     # Get the top n salaries, either with or without duplicates
-    salaries = top_salaries(df)
+    salaries = top_salaries(filtered)
 
     # Calculate the qualifying offer and the standard deviation
     qualifying_offer = average(salaries)
--- qualifying_offer.py
+++ qualifying_offer.py
@@ -1,17 +1,18 @@
 import click
+import numpy as np
 
-from average_salary import get_salary_data, top_salaries, average, standard_deviation
+from average_salary import get_salary_data, filter_year, top_salaries, average, standard_deviation
 
 
 @click.group()
-@click.option('-t', 'top', type=int, default=125,
-              help='Number of salaries to average. Defaults to 125.')
-@click.option('--no-duplicates', 'no_duplicates', is_flag=True,
-              help='Flag that determines whether to include duplicates or use unique values. Default uses duplicates.')
+@click.option('-t', '--top', type=int, default=125, show_default=True,
+              help='Number of salaries to average.')
+@click.option('-y', '--year', type=int, default=2016, show_default=True,
+              help='Year to calculate qualifying offer for.')
 @click.pass_context
-def cli(ctx, top, no_duplicates):
+def cli(ctx, top, year):
     ctx.obj['top'] = top
-    ctx.obj['duplicates'] = not no_duplicates
+    ctx.obj['year'] = year
 
 
 @cli.command('calculate')
@@ -20,15 +21,23 @@ def calculate(obj):
     # Retrieve the salary data from the database
     df = get_salary_data()
 
-    # Get the top n salaries, either with or without duplicates
-    salaries = top_salaries(df, obj['top'], obj['duplicates'])
+    # Remove dataframe rows not equal to given year
+    filtered = filter_year(df, obj['year'])
+
+    # Get the top n salaries
+    salaries = top_salaries(filtered, obj['top'])
 
     # Calculate the qualifying offer and the standard deviation
     qualifying_offer = average(salaries)
     stddev = standard_deviation(salaries)
 
+    qo_str = ('There is not enough data to calculate a qualifying offer for the given parameters.\n'
+        + '[year = {year}, number of salaries to average = {top}]').format(**obj)
+    if qualifying_offer is not np.nan:
+        qo_str = 'The qualifying offer is ${:,} \xB1 ${:,}'.format(qualifying_offer, stddev)
+
     # Display the data
-    print('The qualifying offer is ${:,} \xB1 ${:,}'.format(qualifying_offer, stddev))
+    print(qo_str)
 
 
 if __name__ == '__main__':
